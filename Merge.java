d8851dcb1166eecacbd1b3469dac9401cd817fcf
cwu155 <cwu17@stuy.edu>
2019-03-21 07:20:38 -0400

    yikes

--- Merge.java
+++ Merge.java
@@ -3,19 +3,37 @@ public class Merge{
    public static int lo, hi;
    public static int[] temp;
 
-   public static void mergesort(int[]data, int[]temp, int lo, int hi){
 
-     if (lo >= hi){
-      return;
+   public static void insertionSort(int[] data, int o, int c){
+     int orig = o;
+     int current = c;
+     for (int i = 1; i < data.length; i++){
+       orig = i-1;
+       current = data[i];
+       while (orig >= 0 && current < data[orig]){
+          data[orig+1] = data[orig];
+          orig = orig-1;
+         }
+         data[orig+1] = current;
+       }
      }
 
+   public static void mergesort(int[]data, int[]temp, int lo, int hi){
 
-    int m = (lo + hi + 1) / 2;
-      mergesort(data, temp, lo, m-1);
-      mergesort(data, temp, m, hi);
+    if((hi-lo) <= 120){
+      insertionSort(data, lo, hi);
+      return;
+    }
+
+    int m = (lo + hi) / 2;
+    int l = lo;
+    
+      mergesort(temp, data, lo, m-l+1); //merge sorts the left
+      mergesort(temp, data, hi-m, hi); //merge sorts the right
 
     for (int i = lo; i < hi + 1; i++){
-      if(lo >= m || m < hi + 1 && temp[lo] > temp[m]){
+      if (lo >= (l + hi + 1)/2 || m < hi + 1 && temp[lo] > temp[m]){ //if the middle is within the lower and upper bound,
+                                                                    //and checks to see which elements are lower.
         data[i] = temp[m];
         m++;
       } else {
@@ -27,7 +45,6 @@ public class Merge{
     }
 
   public static void mergesort(int[]data){
-
     mergesort(data, temp, 0, data.length-1);
   }
 
